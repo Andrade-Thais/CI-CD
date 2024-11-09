af3236ce8f7d4c76248c1dcc0a377084837347df
andrade-thais <thais.andrade11@fatec.sp.gov.br>
2024-11-09 09:04:07 -0300

    teste integração

---
+++ test/example.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('exemplo de teste', ()=>{
+
+    it('deve somar dois números corretamente', ()=>{
+
+        assert.strictEqual(1 + 1, 2);
+    });
+});
